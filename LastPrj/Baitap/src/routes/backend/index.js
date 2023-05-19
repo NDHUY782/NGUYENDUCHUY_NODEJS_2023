5f0bae30bf1ba391f9e113785888af48d06f4c9b
DHuy <huynguyen07080401@gmail.com>
2023-05-19 19:25:08 +0700

    last

---
+++ LastPrj/Baitap/src/routes/backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.use((req, res, next) => {
+    req.app.set('layout', 'backend/index.ejs');
+    next();
+});
+
+router.use('/',require('./dashboard'))
+router.use('/category',require('./category'))
+router.use('/article',require('./article'))
+router.use('/rss',require('./rss'))
+
+router.use('/setting',require('./setting'))
+router.use('/menu',require('./menu'))
+
+router.use('/slider',require('./slider'))
+
+
+
+router.use('/sendmail',require('./send_mail'))
+
+router.use('/product',require('./product'))
+// router.use('/coupon',require('./coupon'))
+
+router.use('/category_product',require('./category_product'))
+
+router.use('/account',require('./account'))
+
+
+
+module.exports = router
